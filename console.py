328ced5146e0b644c6e110adb7b53a7a36399377
LCobham <lucascobham@hotmail.com>
2023-03-20 11:18:16 -0700

    Modified console and added setup files for SQL databases

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,13 +115,47 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        #Separate all params
+        arg_list = args.split(" ")
+
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+
+        #Check if first argument is a valid class name
+        elif arg_list[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+
+        #Create a dictionary with all the params
+        dictionary = {}
+        for param in arg_list[1:]:
+            #Param structure: <key>=<value>
+            keyValue = param.partition("=")
+            #Check if param structure is correct, else skip.
+            if len(keyValue[1]) == 1:
+                key = keyValue[0]
+                value = keyValue[2]
+
+                #Parse value: can be either a str, an int or float.
+                #Else skip
+                if value.isdigit():
+                    value = int(value)
+                elif value.count(".") == 1 and \
+                value.partition(".")[0].isdigit() and \
+                value.partition(".")[2].isdigit():
+                    value = float(value)
+                elif not value[0] == value[-1] == '"':
+                    continue
+                else:
+                    value.replace("_", " ")
+                    value = value[1:-1]
+
+                dictionary[key] = value
+
+        #Create new instance and add the parsed params to it
+        new_instance = HBNBCommand.classes[arg_list[0]]()
+        new_instance.__dict__.update(dictionary)
         storage.save()
         print(new_instance.id)
         storage.save()
@@ -272,7 +306,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +314,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
