b731c58c54f625be973d6863d147c39fa72d84e9
LCobham <lucascobham@hotmail.com>
2023-04-01 17:49:03 -0700

    Created tasks 1-4, small change in task 0

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,6 +3,7 @@
     This module creates a web app with Flask
 """
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -10,6 +11,16 @@ app = Flask(__name__)
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    return f"C {escape(text.replace('_', ' '))}"
 
 
 if __name__ == "__main__":
